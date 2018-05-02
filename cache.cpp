481f408495985a7e936cf4eb742a86a996d7175f
andrew <asnolin@uvm.edu>
2018-05-02 18:15:48 -0400

    before group work

--- cache.cpp
+++ cache.cpp
@@ -31,6 +31,11 @@ enum c_type {instr, data, uni};
 enum writeBack {fifo, writeThru, writeback};
 
 
+
+
+
+
+
 //represents one level of cache
 //has ints, a enum, and a struct that represents a block, a vecter 
 class CacheLevel{
@@ -42,7 +47,8 @@ private:
   c_type type;
   int mapping; //number of blocks per set
   int id = idSeed++;
-
+  int blockHit;
+  int blockMisss;
 
   struct block{
     int blockNum;
@@ -81,7 +87,7 @@ public:
   //methods
   
   //true if hit, otherwise false
-  bool find(long int target){
+  bool find(int64_t target){
     bool found = false;
     //determine set 
     //determine block
@@ -92,19 +98,28 @@ public:
   
   //clears all cache
   void clear(){
-    //TODO
+    
   }
 };//end CacheLevel
 
 
 
+
+
+
+
+
+
+
 //contains cachelevel instances. handles passing between cache levels, associativity, and write stratagy
 class Cache{
+private:
   vector <CacheLevel> cVec;
   int numLevels;
-  int numHits;
-  int numMiss;
-  //constructors
+  int cacheHit;
+  int cacheMiss;
+
+public:
   Cache(){
     cVec.push_back(CacheLevel(1,instr));
     cVec.push_back(CacheLevel(1,data));
@@ -126,26 +141,39 @@ class Cache{
   //returns a CacheLevel
   CacheLevel getC(int id){
     for(int i = 0; i < cVec.size(); i++){
-
+      cVec[i].clear();
     }
   }
+
+  //
+  void test(string req){
+
+  }
 };//end Cache
+
+
+
+
+
 
 
   //crawls through directory to read config and trace files
 class Crawler{
   
-  private:
-  string dir = "stuff/";
-  string config = "config/";
-  string trace = "trace/";
-  //config file 
-  string configName;
+private:
+
+  vector <string> reqs; //holds data read from config file
+  string dir = "stuff/";//head of dir
+  string config = "config/";//config dir
+  string trace = "trace/";//trace dir
+  string configName;//config filename
+
   //prefix and suffix of trace files
   string t_prefix = "gcc-addrs-10K-";
   string t_suffix = ".trace";
-  string vers[6] = {"a","b","c","d","e","f"};
-  static int testNum;
+  string vers[] = {"a","b","c","d","e","f"};
+  int maxTests = sizeof(vers)/sizeof(vers[0]);
+  int testNum = 0;//holds number of tests done to determine which vers to use
   
 public:
   Crawler(){
@@ -154,21 +182,37 @@ public:
   Crawler(string inName){
     configName = inName;
   }
+
+
+  //getters
+  vector<string> getReqs(){
+    return reqs;
+  }
+
+
   //methods
   
   //takes in a CacheLevel to assign from config
   void assign(CacheLevel &inLevel){
     
   }
+
+  void incTest(){
+    if(testNum < maxTests){
+      testNum++;
+    }
+  }
+
+
 };//end Crawler
 
 
+
   //set statics
-int Crawler::testNum = 0;
 int CacheLevel::idSeed = 0;
 
   
-//from
+//from cs222-project-02.pdf
 static int64_t hexstrToInt64(string hexstr) {
   return stol(hexstr, nullptr, 16);
 }
@@ -179,6 +223,7 @@ static int64_t hexstrToInt64(string hexstr) {
 int main(int argc, char *argv[]) {
     string inName;
     vector <string> inReq;
+
   //parse arguments
   if(argc > 1){
     inName = argv[1];
@@ -186,10 +231,20 @@ int main(int argc, char *argv[]) {
     inName = "base.config";
   }
   
-  //init 
-
+  //init crawler, cache
+  Cache c;
   Crawler crawl(inName);
-  inReq = crawl.getReqed();
+
+  //while there are tests to do, pass to inReq and do tests
+  while( crawl.get_testNum() != 0){
+    inReq = crawl.getReqs();
+    //while inReq is not empty, pop off req from inReq, and record its hits/misses
+    while(inReq.size() != 0){
+      c.test(inreq.front());
+      inVec.erase(inVec.begin());
+    }
+    crawl.incTest();
+  }
   return 0;
 }
 
