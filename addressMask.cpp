8fa346e3236d322e13f41d7597e0d6cd29ebde54
ternst61 <then.ernst@gmail.com>
2018-05-02 20:50:24 -0400

    adds getIndex fxn with mask

--- addressMask.cpp
+++ addressMask.cpp
@@ -8,30 +8,57 @@
 using namespace std;
 
 //get INDEX value given Cache Size, Set Associativity, Block Size
-int getIndex(int size, int ways, int block){
+int getIndexLength(int size, int ways, int block){
 
 	int denom = block * ways;
 	int result = size / denom;
 	
-	int index = log2 (result);
+	int indexLength = log2 (result);
 	
 	//testing
 	//cout << "\n" << index << "\n";
 }
 
-int getTag(int size, int index, int offset){
-	int tag = size - index - offset;
+int getTagLength(int size, int indexLength, int offsetLength){
+	int tagLength = size - indexLength - offsetLength;
 }
 
-int64_t indexMask(int index, int64_t address){
+int64_t getIndex(int indexLength, int offsetLength,  int64_t address){
 	
+	//initialize mask to 0
+	int64_t mask = 0;
+	
+	//set mask based on length of index
+	for (int i = offsetLength; i < indexLength + offsetLength; i++){
+		mask += pow(2, i);
+	}
+	
+	//mask & index to isolate index address	
+	int64_t index = (address & mask);	
 
+	//test output
+	cout << "\n" << "mask is: " << mask << "\n";
+	cout << "\n" << "index address is: " << index << "\n";
 }
+
+static int64_t hexstrToInt64(std::string hexstr) {
+	return std::stol(hexstr, nullptr, 16);
+}		
+
 
 int main(int argc, char *argv[]) {
 	
-	//testing
-	getIndex(65536, 2, 64);
+	//testing indexLength
+	getIndexLength(65536, 2, 64);
+
+	string hexstr = "7fffe7fefc8";
+
+	int64_t hexString = hexstrToInt64(hexstr);
+
+	cout << "\n" << "hexString is: " << hexString << "\n";
+
+	//testing index
+	getIndex(16, 4, hexString); 
 
 	return 0;
 
