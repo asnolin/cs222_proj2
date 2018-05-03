23d7615793d758159b8f4171666dc2393ee5aa5b
ternst61 <then.ernst@gmail.com>
2018-05-02 21:28:33 -0400

    combined addressMask and cache.cpp

--- cache.cpp
+++ cache.cpp
@@ -25,6 +25,7 @@
 #include<vector>
 #include<cstdint>
 #include<sstream>
+#include<math.h>
 using namespace std;
 
 
@@ -102,6 +103,60 @@ public:
   
   //methods
   
+  //get INDEX length given Cache Size, Set Associativity, Block Size
+  int getIndexLength(int size, int ways, int block){
+
+    int denom = block * ways;
+    int result = size / denom;
+    
+    int indexLength = log2 (result);
+    
+    //testing
+    //cout << "\n" << index << "\n";
+  }
+
+  //get INDEX address based on bitmask
+  int64_t getIndex(int indexLength, int offsetLength,  int64_t address){
+    
+    //initialize mask to 0
+    int64_t indexMask = 0;
+    
+    //set mask based on length of index
+    for (int i = offsetLength; i < indexLength + offsetLength; i++){
+      indexMask += pow(2, i);
+    }
+    
+    //(mask & index) to isolate index address 
+    int64_t index = (address & indexMask);  
+
+    //test output
+    //cout << "\n" << "indexMask is: " << indexMask << "\n";
+    //cout << "\n" << "index address is: " << index << "\n";
+  }
+
+  int getTagLength(int size, int indexLength, int offsetLength){
+    int tagLength = size - indexLength - offsetLength;
+  }
+
+  //get Tag address based on bitmask
+  int64_t getTag(int tagLength, int indexLength, int offsetLength, int64_t address){
+    //initialize mask to 0
+    int64_t tagMask = 0;
+
+    //set mask based on length of tag
+    for (int i = offsetLength + indexLength; i < offsetLength + indexLength + tagLength; i++){
+      tagMask += pow(2, i);
+    }
+
+    //(mask & tag) to isolate tag address
+    int64_t tag = (address & tagMask);
+
+    //cout << "\n" << "tagMask is: " << tagMask << "\n";
+    //cout << "\n" << "tag address is: " << tag << "\n";  
+  }
+
+
+
   //true if hit, otherwise false
   bool find(int64_t target){
     bool found = false;
